183253e112701b7f48c1c975dd905c988da851f5
kryweb <dreidemyromain@gmail.com>
2020-03-17 14:50:02 +0100

    Part 27 done

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -4,7 +4,28 @@ if (navigator.clipboard) {
         const $button = document.createElement('button');
         $button.innerHTML = 'Copier';
         $clipboardEl.parentNode.append($button);
+
+        $button.addEventListener(
+            'click',
+            copyToClipboard.bind(this, $clipboardEl, $button)
+        );
     });
+
+
+
+
+
+    function copyToClipboard($clipboardEl, $button) {
+        console.log('Click !');
+        navigator.clipboard
+            .writeText($clipboardEl.getAttribute('data-clipboard'))
+            .then(() => {
+                $button.innerHTML = 'Copié !';
+                setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+            })
+            .catch((err) => console.warn(err));
+    }
+
 } else {
     console.warn("Pas de support :(")
 }
