7fd244ecc0f896c122129f8138877468239757f4
kryweb <dreidemyromain@gmail.com>
2020-03-18 12:11:28 +0100

    Part 33 done

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
 self.addEventListener('install', (event) => {
     console.log('Hello from the service worker')
 });
+
+addEventListener('fetch', (event) => {
+    if (event.request.headers.get('Accept').includes('text/html')) {
+        event.respondWith(fetch(event.request));
+    }
+});
+
